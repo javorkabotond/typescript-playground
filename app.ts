d9ec804865fcf31729cbfbc5bb9b3e0ce05b34e3
javorkabotond <botondjvorka@gmail.com>
2020-07-02 20:47:05 +0200

    Object

--- app.ts
+++ app.ts
@@ -1,16 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if(showResult){
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+//const person {
+//  name: string;
+//  age: number;
+//} = {
+const person = {
+  name: 'John', 
+  age: 30
 }
 
-let number1: number;
-number1 = 5;
-const number2 = 2.0;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
