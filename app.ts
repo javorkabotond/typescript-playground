3186e3bf246c7611b295e5cd6849e687602f81d1
javorkabotond <botondjvorka@gmail.com>
2020-07-03 15:02:35 +0200

    Array

--- app.ts
+++ app.ts
@@ -4,7 +4,17 @@
 //} = {
 const person = {
   name: 'John', 
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cookiong']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sprots'];
+
+
+
 console.log(person.name);
+
+for(const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+}
